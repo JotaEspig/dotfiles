fb97a088f62417dad155f9bdc8398c01a3a1be8c
João Vitor Espig <jotaespig@gmail.com>
2026-04-21 11:58:09 -0300

    update

--- dot_config/hypr/executable_wallpaper-switch.sh
+++ dot_config/hypr/executable_wallpaper-switch.sh
@@ -6,13 +6,13 @@ WALLPAPER_PATH=$HOME/.config/hypr/wallpapers
 
 cd $WALLPAPER_PATH || exit
 
-SELECTED_WALLPAPER=$(for theme in *.jpg *.png *.gif; do echo -en "$theme\0icon\x1f$theme\n" ; done | rofi -i -dmenu -p "" -theme ~/.config/rofi/wallpaperswitch.rasi)
+SELECTED_WALLPAPER=$(for theme in *.jpg *.png; do echo -en "$theme\0icon\x1f$theme\n" ; done | rofi -i -dmenu -p "" -theme ~/.config/rofi/wallpaperswitch.rasi)
     
 if [ -n "$SELECTED_WALLPAPER" ]; then
-    swww img -t center --transition-duration 3 --transition-fps 60 "$WALLPAPER_PATH/$SELECTED_WALLPAPER"
-    echo "$SELECTED_WALLPAPER" > $HOME/.local/share/themes/$CURRENT_THEME/wall.set
+    awww img -t center --transition-duration 3 --transition-fps 60 "$WALLPAPER_PATH/$SELECTED_WALLPAPER"
+    echo "$SELECTED_WALLPAPER" > $HOME/.config/hypr/wallpapers/current.set
     ## set rofi background image
-    cp "$WALLPAPER_PATH/$SELECTED_WALLPAPER" $HOME/.config/rofi/backgrounds/current_wallpaper.png
+    cp "$WALLPAPER_PATH/$SELECTED_WALLPAPER" $HOME/.config/rofi/current_wallpaper.png
 
 fi
 
