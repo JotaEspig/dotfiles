3fb801a29406b6c23c58ca5c309764f9b0c353f3
João Vitor Espig <jotaespig@gmail.com>
2026-05-10 22:36:34 -0300

    update

---
+++ dot_zsh/aliases.zsh
@@ -0,0 +1,5 @@
+alias htb="sudo openvpn"
+alias ls="ls --color --group-directories-first -X"
+alias la="ls -lah --color"
+alias lh="ls -lh --color"
+alias discord='XDG_SESSION_TYPE=x11 /usr/bin/discord'
